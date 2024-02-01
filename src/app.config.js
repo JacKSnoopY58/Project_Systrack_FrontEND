c0d36d02eb2d0a94ce0e33cad6a23feba47010d1
JacKSnoopY58 <titijack58@gmail.com>
2024-02-02 00:14:22 +0700

    Update app.config.js

--- src/app.config.js
+++ src/app.config.js
@@ -1,5 +1,5 @@
 // export const SERVER_URL = "http://localhost:4080/api/";
-export const SERVER_URL = "https://systrack.onrender.com/api/";
+export const SERVER_URL = "https://systrack-4zml.onrender.com/api/";
 
 // export const SERVER_URL =  "https://systrack.netlify.app/api/";
 
